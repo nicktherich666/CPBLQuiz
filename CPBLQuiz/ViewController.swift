724e5596a7d2ae627b154fb9cc7beb44986085a6
Nick Lu <weiwei19910415@gmail.com>
2023-12-14 19:28:46 +0800

    Initial Commit

---
+++ CPBLQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CPBLQuiz
+//
+//  Created by Nick Lu on 2023/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
